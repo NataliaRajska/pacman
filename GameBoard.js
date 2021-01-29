1b8dfb543e8398d5e88bc18381a55ebee2e6fb2a
NataliaRajska <natalia.rajskaa@gmail.com>
2021-01-29 13:58:42 +0100

    Init class GameBoard and shoGameStatus function

---
+++ GameBoard.js
@@ -0,0 +1,17 @@
+import { GRID_SIZE CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from "./setup";
+
+export class GameBoard {
+
+    constructor(DOMGrid) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid = DOMGrid;
+    }
+
+    showGameStatus(gameWin){
+        const div = document.createElement('div');
+        div.classList.add('game-status');
+        div.innerHTML = `${gameWin ? 'WIN!' : 'GAME OVER'}`;
+        this.DOMGrid.appendChild(div);
+    }
+}
