28aaa743720ffe062f4c66d6d884cda922c27fa2
NataliaRajska <natalia.rajskaa@gmail.com>
2021-01-29 14:09:41 +0100

    Create function: createGrid, add and remove object

--- GameBoard.js
+++ GameBoard.js
@@ -1,4 +1,4 @@
-import { GRID_SIZE CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from "./setup";
+import {GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST} from "./setup";
 
 export class GameBoard {
 
@@ -8,10 +8,37 @@ export class GameBoard {
         this.DOMGrid = DOMGrid;
     }
 
-    showGameStatus(gameWin){
+    showGameStatus(gameWin) {
         const div = document.createElement('div');
         div.classList.add('game-status');
         div.innerHTML = `${gameWin ? 'WIN!' : 'GAME OVER'}`;
         this.DOMGrid.appendChild(div);
     }
+
+    createGrid(level) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid.innerHTML = '';
+        this.DOMGrid.style.cssText = `grid-template-colums: repeat(${GRID_SIZE},${CELL_SIZE}px)`;
+
+        level.forEach((square, i) => {
+            const div = document.createElement('div');
+            div.classList.add('square', CLASS_LIST[square]);
+            div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px`;
+            this.DOMGrid.appendChild(div);
+            this.grid.push(div);
+
+            if(CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
+        });
+    }
+
+    addObject(pos, classes){
+        this.grid[pos].classList.add(...classes);
+    }
+
+    removeObject(pos, classes){
+        this.grid[pos].classList.remove(...classes);
+    }
+
+
 }
