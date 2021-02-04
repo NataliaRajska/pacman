879eba9ae0f92fc5eb8fc8353f75f584062cd672
NataliaRajska <natalia.rajskaa@gmail.com>
2021-02-04 16:38:18 +0100

    Put ghost on the board

--- Ghost.js
+++ Ghost.js
@@ -2,10 +2,10 @@ import {DIRECTIONS, OBJECT_TYPE} from "./setup";
 
 class Ghost {
     constructor(speed = 5, startPos, movement, name) {
-        this.startPos = startPos;
-        this.pos = startPos;
         this.name = name;
         this.movement = movement;
+        this.startPos = startPos;
+        this.pos = startPos;
         this.dir = DIRECTIONS.ArrowRight;
         this.speed = speed;
         this.timer = 0;
@@ -23,20 +23,26 @@ class Ghost {
     }
 
     getNextMove(objectExist) {
-        const {nextMovePos, direction} = this.movement(this.pos, this.dir, objectExist);
+        const {nextMovePos, direction} = this.movement(
+            this.pos,
+            this.dir,
+            objectExist
+        );
         return {nextMovePos, direction}
     }
 
     makeMove() {
         const classesToRemove = [OBJECT_TYPE.GHOST, OBJECT_TYPE.SCARED, this.name];
-        let classesToAdd = [OBJECT_TYPE.GHOST, this, name];
+        let classesToAdd = [OBJECT_TYPE.GHOST, this.name];
 
         if (this.isScared) classesToAdd = [...classesToAdd, OBJECT_TYPE.SCARED];
         return {classesToRemove, classesToAdd};
     }
 
-    setNewPos(nextMovePos,direction) {
-         this.pos= nextMovePos;
-         this.dir = direction;
+    setNewPos(nextMovePos, direction) {
+        this.pos = nextMovePos;
+        this.dir = direction;
     }
 }
+
+export default Ghost;
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import {LEVEL, OBJECT_TYPE} from "./setup";
 import {randomMovement} from "./ghostMoves";
-//classes
+// classes
 import GameBoard from './GameBoard';
 import Pacman from './Pacman';
+import Ghost from "./Ghost";
 
-//DOM elements
+// DOM elements
 const gameGrid = document.querySelector('#game');
 const scoreTable = document.querySelector('#score');
 const startButton = document.querySelector('#start-button');
 
-//Game Constants
+// Game Constants
 const POWER_PILL_TIME = 10000; //ms
 const GLOBAL_SPEED = 80; //ms
 const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
 
-//Initial Setup
+// Initial Setup
 let score = 0;
 let timer = null;
 let gameWin = false;
@@ -32,6 +33,7 @@ function checkCollision(pacman, ghosts) {
 
 function gameLoop(pacman, ghosts) {
     gameBoard.moveCharacter(pacman);
+    ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
 }
 
 function startGame() {
@@ -48,7 +50,15 @@ function startGame() {
         pacman.handleKeyInput(e, gameBoard.objectExist)
     );
 
-    timer = setInterval(() => gameLoop(pacman), GLOBAL_SPEED);
+    const ghosts = [
+        // posiotion in the ghostbox
+        new Ghost(5, 188, randomMovement, OBJECT_TYPE.BLINKY),
+        new Ghost(4, 209, randomMovement, OBJECT_TYPE.PINKY),
+        new Ghost(3, 230, randomMovement, OBJECT_TYPE.INKY),
+        new Ghost(2, 251, randomMovement, OBJECT_TYPE.CLYDE)
+    ];
+
+    timer = setInterval(() => gameLoop(pacman, ghosts), GLOBAL_SPEED);
 }
 
 startButton.addEventListener('click', startGame);
